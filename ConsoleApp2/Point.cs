9aaea0c7dc2b741c823b906ea4cfa3e6c1a67877
johndward01 <johndward01@gmail.com>
2023-02-16 15:51:06 -0600

    Add project files.

---
+++ ConsoleApp2/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2;
+internal class Point
+{
+    public Point(double x, double y)
+    {
+        X = x; 
+        Y = y;
+    }
+
+    public double X;
+    public double Y;
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleApp2;
+
+static double DistanceBetweenPoints(Point a, Point b)
+{
+    var distance = Math.Sqrt(Math.Pow((a.X - b.X), 2) + Math.Pow((a.Y - b.Y), 2));
+    return distance;
+}
+
+var distance = DistanceBetweenPoints(new Point(-10.2, 12.5), new Point(0.3, 14.7));
+Console.WriteLine(distance);
